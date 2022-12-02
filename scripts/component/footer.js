243a6f7010e66d4d50e6ae29a2c682b254c253c6
Agil Adi Saputro <agiladisaputro@gmail.com>
2022-12-02 09:13:41 +0700

    Initial push

---
+++ scripts/component/footer.js
@@ -0,0 +1,45 @@
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <ul class="list-inline mb-2">
+                            <li class="list-inline-item"><a href="visi-misi.html">Visi Misi</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="struktur-organisasi.html">Struktur Organisasi</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="produk-pemasaran.html">Produk Pemasaran</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="kontrak-kerjasama.html">Kontrak Kerjasama</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="csr.html">CSR</a></li>
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2022. All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        `
+    }
+}
+
+customElements.define('custom-footer', Footer);
---
+++ scripts/component/navbar.js
@@ -0,0 +1,17 @@
+class NavBar extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <nav class="navbar navbar-light bg-light static-top">
+            <a class="navbar-brand" href="/">
+                <img src="/assets/img/logo.png" width="150" height="" alt="">
+            </a>
+        </nav>
+        `
+    }
+}
+
+customElements.define('nav-bar', NavBar);
